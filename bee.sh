f3aa58f223b34fba124beb34dba1d05ed7e561fe
bianfusia <44915719+bianfusia@users.noreply.github.com>
2022-06-01 11:05:29 +0800

    Update bee.sh

--- bee.sh
+++ bee.sh
@@ -24,4 +24,6 @@ gunzip /usr/share/wordlists/rockyou.txt.gz > /dev/null 2>&1
 echo "${GREEN}[+] Cloning common CTF wordlists to folder...${NC}"
 cp /usr/share/wordlists/rockyou.txt rockyou.txt > /dev/null 2>&1
 cp /usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt directory-list-2.3-medium.txt > /dev/null 2>&1
+sudo apt install remmina -y > /dev/null 2>&1
+echo "${GREEN}[+] Installing RDP software remmina...${NC}"
 echo "${GREEN}[+][+][+][+] YOUR KALI BOX IS READY TO CTF! [+][+][+][+]${NC}"
