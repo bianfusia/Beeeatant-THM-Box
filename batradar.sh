1d92b46ca9a99ed310182cbded1850ef1965cdde
bianfusia <44915719+bianfusia@users.noreply.github.com>
2023-01-05 09:12:03 +0800

    Update batradar.sh

--- batradar.sh
+++ batradar.sh
@@ -7,7 +7,8 @@ echo "*******developed by: @beeeatant**************"
 find ./ -type f > allfiles.txt
 
 while IFS= read -r line; do
-	if cat "$line" | grep "password" > /dev/null 2>&1; then
-		echo "[+] $line may contain password"
+	if cat "$line" | grep -i "password" > /dev/null 2>&1; then
+		echo "[+] $line may contain password" | tee -a findings.txt
+		cat "$line" | grep -i -o -P --color=always '.{0,30}password.{0,30}' | tee -a findings.txt
 	fi
 done < allfiles.txt
